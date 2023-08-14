385c4650b0929da17893cbcdc295457dbc7127a9
Jitesh Rawat <73243338+JiteshRawat@users.noreply.github.com>
2023-08-14 22:19:07 +0530

    update to debug

--- app.py
+++ app.py
@@ -331,132 +331,103 @@ if st.button("Search") and input_value != "":
     # st.write("Result:", result)
     placeholder.empty()
 
-    # top_products= pd.read_csv("data/top_products_by_ratings.csv")
-    # amazon_data= pd.read_csv("data/amazon_data.csv")
-    best_sellers= amazon_data[amazon_data["is_best_seller"] == True].reset_index(drop=True)
-
-    column = st.columns([1, 1, 1])
-    if len(best_sellers) > 0:
-        ###################################### Best Seller layout ###############################################
-        image_link= best_sellers["img_link"][0]
-        title= best_sellers["title"][0].split(",")[0].split("|")[0] 
-        # column[0].markdown(f'''<div style= "background-color: #f7f7f7; padding-left: 20px;"> </div>''', unsafe_allow_html=True)
-        column[0].markdown(f'''<div style="{card_style1}">
-                            <h4 style= "color: #FF9900; "> Bestseller </h4>
-                           <a style= "color: grey;" href= "{best_sellers["product_link"][0]}"/> <p style= "font-size: 18px;"> {title}  </p>
-                           <img src="{image_link}" alt="Amazon Product Image" style="height: 170px"> </div>''',unsafe_allow_html=True)
-
-        plot_best_seller_rating(column[1])
-
-        header_css= " margin-bottom: -20px; text-align: center; color: grey;"
-
-        column[2].markdown(f'''<div style="{card_style1}">
-                            <h4 style= "color: #FF9900; padding-left: 20px;"> Best seller ratings and reviews </h4>
-                            <h4 style= "{header_css}"> Total ratings </h4>
-                            <h1 style= "color: grey; text-align: center; margin-bottom: -10px;">  {int(best_sellers["total_ratings"][0])}  </h2>
-                            <h4 style= "{header_css}"> Total reviews </h4> 
-                            <h1 style= "color: grey; text-align: center; margin-bottom: -10px;"> {int(best_sellers["total_reviews"][0])}  </h2>
-                             </div>''',unsafe_allow_html=True)
-
-    else:
-        st.markdown(f'''<div style="{card_style1}">Oops looks like there was no Best seller in results </div>''',unsafe_allow_html=True)
-
-
-
-    column = st.columns([3, 1])
-    ###################### Top products by rating #########################################
-    card_style = """padding: 15px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); background-color: #f7f7f7;"""
-    column[0].markdown(f'''<div> <h4 style= "color: #FF9900; {card_style}"> Top products by rating </h4> </div>''', unsafe_allow_html=True)    
-
-    html_table = f"""
-    <table style="background-color: #f7f7f7; width:100%">
-      <tr><td> <a style= "color: grey;" href= "{top_products["product_link"][0]}"/> {top_products["title"][0]}</td><td>{top_products['rating'][0]}</td></tr>
-      <tr><td> <a style= "color: grey;" href= "{top_products["product_link"][1]}"/> {top_products["title"][1]}</td><td>{top_products['rating'][1]}</td></tr>
-      <tr><td> <a style= "color: grey;" href= "{top_products["product_link"][2]}"/> {top_products["title"][2]}</td><td>{top_products['rating'][2]}</td></tr>
-      <tr><td> <a style= "color: grey;" href= "{top_products["product_link"][3]}"/> {top_products["title"][3]}</td><td>{top_products['rating'][3]}</td></tr>
-      <tr><td> <a style= "color: grey;" href= "{top_products["product_link"][4]}"/> {top_products["title"][4]}</td><td>{top_products['rating'][4]}</td></tr>
-    </table>
-    """
-
-    # Render the HTML table using Markdown
-    column[0].markdown(html_table, unsafe_allow_html=True)
-
-    ########################### Top products average price ############################
-    column[1].markdown(f'''<div style="{card_style1}"> 
-                       <h4 style= "color: #FF9900; margin-top: -10px; text-align: center;"> Average price </h4>
-                       <p style= "margin: -20px 0px -10px 0px; text-align: center; "> ( Top products by rating ) </p>
-                       <h1 style= "color: grey; text-align: center; font-size: 50px"> {round(top_products["price"].mean())}</h1>
-                       <h6 style= "text-align: center; margin-top: -20px; margin-bottom: -30px "> INR </h6>
-                       </div>''',unsafe_allow_html=True)
-
-    column[1].markdown(f'''<div style="margin-top : 10px; {card_style1}"> 
-                       <h4 style= "color: #FF9900; margin-top: -10px; text-align: center;"> Average Rating </h4>
-                       <p style= "margin: -20px 0px -30px 0px; text-align: center; "> ( Top products by rating ) </p>
-                       <h1 style= "color: grey; text-align: center; font-size: 50px"> {round(top_products["rating"].mean())}</h1>
-                       </div>''',unsafe_allow_html=True)
-
-
-
-
-    column = st.columns([1,2,1])
-    ###################### Distribution plot for price ########################################
-    column[1].markdown(f'''<div style= "margin : 10px 0 -50px 0; {card_style}"> 
-                       <h4 style= "color: #FF9900; margin-top: -20px;">Product distribution by price</h4></div>''', unsafe_allow_html=True)
-    fig,ax = plt.subplots()
-    fig.set_size_inches(6, 2)
-    plt.tick_params(left=False, right=False, labelleft=False, labelbottom=True)
-    # ax.set_facecolor('none')
-    for spine in ax.spines.values():
-        spine.set_visible(False)
-
-    counts, edges, bars = ax.hist(amazon_data["price"], bins= 15 ,color="#FF9900")
-
-    ax.set_ylabel("Number of products", fontsize = 10, fontweight ='bold')
-    ax.set_xlabel("Price")
-    plt.box(False)
-    def format_func(value, tick_number):
-        if value > 1000:
-            K_value = float(value / 1000)
-            return f"{K_value}K"
-        return round(value)
-
-    ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
-    fig.set_facecolor('#f7f7f7')  # Set figure background color to black
-    ax.set_facecolor('#f7f7f7') 
-
-    column[1].pyplot(fig)
-
-
-    ###################### Positive vs Negative plot ########################################
-    column[0].markdown(f'''<div style= "margin : 10px 0 -40px 0; {card_style} padding-top: -20px;"> 
-                       <h5 style= "color: #FF9900;"> Positive vs Critical reviews  </h5></div>''', unsafe_allow_html=True)
-    fig,ax = plt.subplots()
-    plt.tick_params(left=False, right=False, labelleft=False, labelbottom=True)
-
-    x= ["positive", "critical"]
-    y= [round(amazon_data["total_positive_reviews"].mean()), 
-        round(amazon_data["total_critical_reviews"].mean())]
-    plt.bar(x, y,color=["#FF9900", "lightgrey"] ) 
-    plt.ylabel("Number of reviews", fontsize = 20 )
-    plt.xticks(fontsize=20)
-    fig.set_facecolor('#f7f7f7')  # Set figure background color to black
-    ax.set_facecolor('#f7f7f7') 
-    plt.box(False)
-    for i in range(len(x)):
-        plt.text(i,y[i],y[i], ha= "center", fontsize= 20)
-
-
-    column[0].pyplot(fig)
-
-    #################################### Average cards ############################################
-    column[2].markdown(f'''<div style="margin-top: 10px; {card_style1} "> 
-                       <h4 style= "color: #FF9900; margin-top: -10px; text-align: center;"> Average Positive Reviews </h4>
-                       <h1 style= "margin: -40px 0 -20px; color: grey; text-align: center; font-size: 50px"> {round(top_products["total_positive_reviews"].mean())}</h1>
-                       </div>''',unsafe_allow_html=True)
-
-    column[2].markdown(f'''<div style="margin-top: 7px; {card_style1}"> 
-                       <h4 style= "color: #FF9900; margin-top: -10px; text-align: center;"> Average Critical Reviews </h4>
-                       <h1 style= "margin: -40px 0 -20px; color: grey; text-align: center; font-size: 50px"> {round(top_products["total_critical_reviews"].mean())}</h1>
-                       </div>''',unsafe_allow_html=True)
+top_products= pd.read_csv("./data/top_products_by_ratings.csv")
+amazon_data= pd.read_csv("./data/amazon_data.csv")
+best_sellers= amazon_data[amazon_data["is_best_seller"] == True].reset_index(drop=True)
+column = st.columns([1, 1, 1])
+if len(best_sellers) > 0:
+    ###################################### Best Seller layout ###############################################
+    image_link= best_sellers["img_link"][0]
+    title= best_sellers["title"][0].split(",")[0].split("|")[0] 
+    # column[0].markdown(f'''<div style= "background-color: #f7f7f7; padding-left: 20px;"> </div>''', unsafe_allow_html=True)
+    column[0].markdown(f'''<div style="{card_style1}">
+                        <h4 style= "color: #FF9900; "> Bestseller </h4>
+                       <a style= "color: grey;" href= "{best_sellers["product_link"][0]}"/> <p style= "font-size: 18px;"> {title}  </p>
+                       <img src="{image_link}" alt="Amazon Product Image" style="height: 170px"> </div>''',unsafe_allow_html=True)
+    plot_best_seller_rating(column[1])
+    header_css= " margin-bottom: -20px; text-align: center; color: grey;"
+    column[2].markdown(f'''<div style="{card_style1}">
+                        <h4 style= "color: #FF9900; padding-left: 20px;"> Best seller ratings and reviews </h4>
+                        <h4 style= "{header_css}"> Total ratings </h4>
+                        <h1 style= "color: grey; text-align: center; margin-bottom: -10px;">  {int(best_sellers["total_ratings"][0])}  </h2>
+                        <h4 style= "{header_css}"> Total reviews </h4> 
+                        <h1 style= "color: grey; text-align: center; margin-bottom: -10px;"> {int(best_sellers["total_reviews"][0])}  </h2>
+                         </div>''',unsafe_allow_html=True)
 else:
-    pass
+    st.markdown(f'''<div style="{card_style1}">Oops looks like there was no Best seller in results </div>''',unsafe_allow_html=True)
+column = st.columns([3, 1])
+###################### Top products by rating #########################################
+card_style = """padding: 15px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); background-color: #f7f7f7;"""
+column[0].markdown(f'''<div> <h4 style= "color: #FF9900; {card_style}"> Top products by rating </h4> </div>''', unsafe_allow_html=True)    
+html_table = f"""
+<table style="background-color: #f7f7f7; width:100%">
+  <tr><td> <a style= "color: grey;" href= "{top_products["product_link"][0]}"/> {top_products["title"][0]}</td><td>{top_products['rating'][0]}</td></tr>
+  <tr><td> <a style= "color: grey;" href= "{top_products["product_link"][1]}"/> {top_products["title"][1]}</td><td>{top_products['rating'][1]}</td></tr>
+  <tr><td> <a style= "color: grey;" href= "{top_products["product_link"][2]}"/> {top_products["title"][2]}</td><td>{top_products['rating'][2]}</td></tr>
+  <tr><td> <a style= "color: grey;" href= "{top_products["product_link"][3]}"/> {top_products["title"][3]}</td><td>{top_products['rating'][3]}</td></tr>
+  <tr><td> <a style= "color: grey;" href= "{top_products["product_link"][4]}"/> {top_products["title"][4]}</td><td>{top_products['rating'][4]}</td></tr>
+</table>
+"""
+# Render the HTML table using Markdown
+column[0].markdown(html_table, unsafe_allow_html=True)
+########################### Top products average price ############################
+column[1].markdown(f'''<div style="{card_style1}"> 
+                   <h4 style= "color: #FF9900; margin-top: -10px; text-align: center;"> Average price </h4>
+                   <p style= "margin: -20px 0px -10px 0px; text-align: center; "> ( Top products by rating ) </p>
+                   <h1 style= "color: grey; text-align: center; font-size: 50px"> {round(top_products["price"].mean())}</h1>
+                   <h6 style= "text-align: center; margin-top: -20px; margin-bottom: -30px "> INR </h6>
+                   </div>''',unsafe_allow_html=True)
+column[1].markdown(f'''<div style="margin-top : 10px; {card_style1}"> 
+                   <h4 style= "color: #FF9900; margin-top: -10px; text-align: center;"> Average Rating </h4>
+                   <p style= "margin: -20px 0px -30px 0px; text-align: center; "> ( Top products by rating ) </p>
+                   <h1 style= "color: grey; text-align: center; font-size: 50px"> {round(top_products["rating"].mean())}</h1>
+                   </div>''',unsafe_allow_html=True)
+column = st.columns([1,2,1])
+###################### Distribution plot for price ########################################
+column[1].markdown(f'''<div style= "margin : 10px 0 -50px 0; {card_style}"> 
+                   <h4 style= "color: #FF9900; margin-top: -20px;">Product distribution by price</h4></div>''', unsafe_allow_html=True)
+fig,ax = plt.subplots()
+fig.set_size_inches(6, 2)
+plt.tick_params(left=False, right=False, labelleft=False, labelbottom=True)
+# ax.set_facecolor('none')
+for spine in ax.spines.values():
+    spine.set_visible(False)
+counts, edges, bars = ax.hist(amazon_data["price"], bins= 15 ,color="#FF9900")
+ax.set_ylabel("Number of products", fontsize = 10, fontweight ='bold')
+ax.set_xlabel("Price")
+plt.box(False)
+def format_func(value, tick_number):
+    if value > 1000:
+        K_value = float(value / 1000)
+        return f"{K_value}K"
+    return round(value)
+ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
+fig.set_facecolor('#f7f7f7')  # Set figure background color to black
+ax.set_facecolor('#f7f7f7') 
+column[1].pyplot(fig)
+###################### Positive vs Negative plot ########################################
+column[0].markdown(f'''<div style= "margin : 10px 0 -40px 0; {card_style} padding-top: -20px;"> 
+                   <h5 style= "color: #FF9900;"> Positive vs Critical reviews  </h5></div>''', unsafe_allow_html=True)
+fig,ax = plt.subplots()
+plt.tick_params(left=False, right=False, labelleft=False, labelbottom=True)
+x= ["positive", "critical"]
+y= [round(amazon_data["total_positive_reviews"].mean()), 
+    round(amazon_data["total_critical_reviews"].mean())]
+plt.bar(x, y,color=["#FF9900", "lightgrey"] ) 
+plt.ylabel("Number of reviews", fontsize = 20 )
+plt.xticks(fontsize=20)
+fig.set_facecolor('#f7f7f7')  # Set figure background color to black
+ax.set_facecolor('#f7f7f7') 
+plt.box(False)
+for i in range(len(x)):
+    plt.text(i,y[i],y[i], ha= "center", fontsize= 20)
+column[0].pyplot(fig)
+#################################### Average cards ############################################
+column[2].markdown(f'''<div style="margin-top: 10px; {card_style1} "> 
+                   <h4 style= "color: #FF9900; margin-top: -10px; text-align: center;"> Average Positive Reviews </h4>
+                   <h1 style= "margin: -40px 0 -20px; color: grey; text-align: center; font-size: 50px"> {round(top_products["total_positive_reviews"].mean())}</h1>
+                   </div>''',unsafe_allow_html=True)
+column[2].markdown(f'''<div style="margin-top: 7px; {card_style1}"> 
+                   <h4 style= "color: #FF9900; margin-top: -10px; text-align: center;"> Average Critical Reviews </h4>
+                   <h1 style= "margin: -40px 0 -20px; color: grey; text-align: center; font-size: 50px"> {round(top_products["total_critical_reviews"].mean())}</h1>
+                   </div>''',unsafe_allow_html=True)
